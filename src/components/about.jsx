b5472bb30c654f294f6d16e3983f24adb7a953f8
Reister Ruedas <reister.ruedas@globe.com.ph>
2025-02-16 16:53:21 +0800

    Fix Mobile orientation

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,17 +7,17 @@ function Heading() {
         className="flex flex-col w-[80%] justify-center items-center !overflow-hidden pt-32 font-poppins"
         id="About"
       >
-        <section className="flex flex-row  justify-center items-center ">
+        <section className="flex flex-col md:flex-row  justify-center items-center ">
 
           <img className="w-2/4" src="./profileImage.png" alt="Image was here"></img>
-          <div className="flex flex-col w-[90vw] md:w-[50vw] justify-center items-start gap-5 ">
+          <div className="flex flex-col w-[80vw] md:w-[50vw] justify-center items-start gap-5 ">
             <span className="leading-[1em] whitespace-nowrap gap-3 text-left transition-all duration-500">
               <span className="leading-[1em] text-[40px] font-black lg:text-[100px] text-newbeige transition-all duration-500 ">
                 <span className="text-newmaroon">I'm Reister</span> 
               </span>
               <span className="text-[60px] font-extrabold">.</span>
             </span>
-            <span className="text-left text-2xl font-light lg:self-end text-newbeige transition-all duration-200">
+            <span className="text-left text-sm md:text-2xl font-light lg:self-end text-newbeige transition-all duration-200">
               A Software Engineer based in the Philippines.
               Specializes in building user-friendly and interactive web and
               mobile applications. 
