b338bfa9208720e377b7f4acbc24ea27cb5d9cb2
rlruedas <reister.ruedas@gmail.com>
2026-03-30 03:03:22 +0800

    update header for fast response

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,35 +5,63 @@ import Script from "next/script";
 function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://rsms.me/"></link>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+            <Head>
+        <link rel="preconnect" href="https://rsms.me" />
         <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="preload"
+          href="https://rsms.me/inter/inter.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <noscript>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </noscript>
+      
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        ></link>
+          rel="preload"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          />
+        </noscript>
+      
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
+          rel="preload"
           href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
-          rel="stylesheet"
-        ></link>
-        <link
-          href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css"
-          rel="stylesheet"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
         />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          />
+        </noscript>
+      
         <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        ></link>
+          rel="preload"
+          href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css"
+          />
+        </noscript>
+      
         <Script
           defer
           src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"
-        ></Script>
+        />
       </Head>
       <body>
         <Main />
